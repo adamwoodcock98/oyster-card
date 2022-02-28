09bac272c8f21d7522d68afb61770437513d7bed
Christian Pinna <christian.a.pinna@gmail.com>
2022-02-28 15:31:34 +0000

    added test to increase test by specified amount, and added code to make it work

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -9,4 +9,9 @@ describe Oystercard do
   it 'should have a top-up method that takes one argument' do
     expect(subject).to respond_to(:top_up).with(1).argument
   end 
+
+  it 'should increase balance by specified amount' do 
+    subject.top_up(10)
+    expect(subject.balance).to eq(10) #balance is returnd, this is what we are testing
+  end
 end
